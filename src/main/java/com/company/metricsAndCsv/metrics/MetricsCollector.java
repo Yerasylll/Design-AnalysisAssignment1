c73a6cfb4c57d7101cdb9d050c1b3ad3cee0b04b
Yerasyl Alimbek <erasylalimbek302@gmail.com>
2025-09-27 21:38:39 +0500

    Forgot about MetricsCollector to initialize remaining metric classes

--- src/main/java/com/company/metricsAndCsv/metrics/MetricsCollector.java
+++ src/main/java/com/company/metricsAndCsv/metrics/MetricsCollector.java
@@ -1,4 +1,23 @@
 package com.company.metricsAndCsv.metrics;
 
 public class MetricsCollector {
+    private final TimeMetric time = new TimeMetric();
+    private final ComparisonMetric comparisons = new ComparisonMetric();
+    private final DepthMetric depth = new DepthMetric();
+
+    public TimeMetric time() {
+        return time;
+    }
+    public ComparisonMetric comparisons() {
+        return comparisons;
+    }
+    public DepthMetric depth() {
+        return depth;
+    }
+
+    public void reset() {
+        time.reset();
+        comparisons.reset();
+        depth.reset();
+    }
 }
